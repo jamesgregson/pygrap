05d3b721713f836236dd1e3a63aab5e7faffc56c
james <james.gregson@gmail.com>
2019-01-06 20:02:12 -0800

    started stub of shader handler

---
+++ graphics/opengl/shader.py
@@ -0,0 +1,85 @@
+from OpenGL.GL import *
+
+class Shader(object):
+    """ Helper class for using GLSL shader programs
+
+    adapted from: https://gist.github.com/deepankarsharma/3494203
+    """
+    def __init__(self, vertex, fragment):
+        """
+        Parameters
+        ----------
+        vertex : str
+            String containing shader source code for the vertex
+            shader
+        fragment : str
+            String containing shader source code for the fragment
+            shader
+        """
+        self.program_id = glCreateProgram()
+        vs_id = self.add_shader(vertex, GL_VERTEX_SHADER)
+        frag_id = self.add_shader(fragment, GL_FRAGMENT_SHADER)
+
+        glAttachShader(self.program_id, vs_id)
+        glAttachShader(self.program_id, frag_id)
+        glLinkProgram(self.program_id)
+
+        if glGetProgramiv(self.program_id, GL_LINK_STATUS) != GL_TRUE:
+            info = glGetProgramInfoLog(self.program_id)
+            glDeleteProgram(self.program_id)
+            glDeleteShader(vs_id)
+            glDeleteShader(frag_id)
+            raise RuntimeError('Error linking program: %s' % (info))
+        glDeleteShader(vs_id)
+        glDeleteShader(frag_id)
+
+    def add_shader(self, source, shader_type):
+        """ Helper function for compiling a GLSL shader
+        Parameters
+        ----------
+        source : str
+            String containing shader source code
+        shader_type : valid OpenGL shader type
+            Type of shader to compile
+        Returns
+        -------
+        value : int
+            Identifier for shader if compilation is successful
+        """
+        try:
+            shader_id = glCreateShader(shader_type)
+            glShaderSource(shader_id, source)
+            glCompileShader(shader_id)
+            if glGetShaderiv(shader_id, GL_COMPILE_STATUS) != GL_TRUE:
+                info = glGetShaderInfoLog(shader_id)
+                raise RuntimeError('Shader compilation failed: %s' % (info))
+            return shader_id
+        except:
+            glDeleteShader(shader_id)
+            raise
+
+    def uniform_location(self, name):
+        """ Helper function to get location of an OpenGL uniform variable
+        Parameters
+        ----------
+        name : str
+            Name of the variable for which location is to be returned
+        Returns
+        -------
+        value : int
+            Integer describing location
+        """
+        return glGetUniformLocation(self.program_id, name)
+
+    def attribute_location(self, name):
+        """ Helper function to get location of an OpenGL attribute variable
+        Parameters
+        ----------
+        name : str
+            Name of the variable for which location is to be returned
+        Returns
+        -------
+        value : int
+            Integer describing location
+        """
+        return glGetAttribLocation(self.program_id, name)
